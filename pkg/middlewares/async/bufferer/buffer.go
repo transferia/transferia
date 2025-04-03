a5497c9361759f92f4a8deefcc81523b95b453ce
booec <booec@yandex-team.com>
2025-04-03 17:34:56 +0300

    Optimize bufferer
    For now, bufferer middleware much times grows slice what cause `growSlice` function calls and garbage collector usage and wasting of CPU time
    
    Example for GP-\>GP transfer:
    
    ![telegram-cloud-photo-size-2-5413364629872175926-y.jpg](https://arcanum.s3.mds.yandex.net/files/booec/iAST7iIi6ApouKCJ7WL7S)
    
     ![telegram-cloud-photo-size-2-5413364629872175927-x.jpg](https://arcanum.s3.mds.yandex.net/files/booec/oWnykoxsJP3NHkwJEbSV1)
    commit_hash:4a8e24c4aef366c253ce88c56da8b4df331ef488

--- pkg/middlewares/async/bufferer/buffer.go
+++ pkg/middlewares/async/bufferer/buffer.go
@@ -1,43 +1,62 @@
 package bufferer
 
-import "github.com/transferia/transferia/pkg/abstract"
+import (
+	"github.com/transferia/transferia/pkg/abstract"
+)
 
 type buffer struct {
-	batch           []abstract.ChangeItem
+	// batches represents all batches of items added to buffer.
+	// It is stored as slice of batches instead of slice of changeitems to prevent
+	// unnecessary `growSlice` functions and reallocations on `buffer.Add` calls.
+	batches         [][]abstract.ChangeItem
+	size            int // size is number of all items from batches.
 	batchValuesSize uint64
 	errChs          []chan error
 }
 
 func newBuffer() *buffer {
 	return &buffer{
-		batch:           make([]abstract.ChangeItem, 0),
+		batches:         nil,
+		size:            0,
 		batchValuesSize: 0,
 		errChs:          make([]chan error, 0),
 	}
 }
 
 func (b *buffer) Add(items []abstract.ChangeItem, errCh chan error) {
-	b.batch = append(b.batch, items...)
+	b.batches = append(b.batches, items)
+	b.size += len(items)
 	b.batchValuesSize += valuesSizeOf(items)
 	b.errChs = append(b.errChs, errCh)
 }
 
 func (b *buffer) Flush(flushFn func([]abstract.ChangeItem) error) {
+	var totalBatch []abstract.ChangeItem // totalBatch is one slice of all changeitems from batches.
+	if len(b.batches) == 1 {
+		totalBatch = b.batches[0] // To not copy changeitems.
+	} else {
+		totalBatch = make([]abstract.ChangeItem, 0, b.size)
+		for _, items := range b.batches {
+			totalBatch = append(totalBatch, items...)
+		}
+	}
+
 	var err error = nil
-	if len(b.batch) > 0 {
-		err = flushFn(b.batch)
+	if len(totalBatch) > 0 {
+		err = flushFn(totalBatch)
 	}
 	for _, c := range b.errChs {
 		c <- err
 	}
 
-	b.batch = make([]abstract.ChangeItem, 0)
+	b.batches = nil
+	b.size = 0
 	b.batchValuesSize = 0
 	b.errChs = make([]chan error, 0)
 }
 
 func (b *buffer) Len() int {
-	return len(b.batch)
+	return b.size
 }
 
 func (b *buffer) ValuesSize() uint64 {
