fb4357b2ed75d56f1b1d4d765173356d95da67e5
kry127 <kry127@yandex-team.com>
2025-04-09 17:22:22 +0300

    Introduce prolongable transfer status messages
    1. Introduce conception of prolongable transfer status message (as it was before @in-leskin refactoring): <HIDDEN_URL>
    
    2. Refactored repo a little bit to use explicit transactions `pgx.Tx` , increased reusability of code
    
    3. Inserted main feature: skip closing and reopening of status message: <HIDDEN_URL>. In comparison, what was before:<HIDDEN_URL>
    
    4. Added test for old-new functionality, improved neighbourhood tests as well
    commit_hash:f95139c59bace2d3b30f27ae25397b4d738eb5ab

--- pkg/abstract/coordinator/status_message.go
+++ pkg/abstract/coordinator/status_message.go
@@ -1,6 +1,9 @@
 package coordinator
 
-import "github.com/transferia/transferia/pkg/errors/coded"
+import (
+	"github.com/transferia/transferia/library/go/slices"
+	"github.com/transferia/transferia/pkg/errors/coded"
+)
 
 type StatusMessageType string
 
@@ -16,4 +19,10 @@ type StatusMessage struct {
 	Categories []string          `json:"categories"`
 	Code       coded.Code        `json:"code"`
 	ID         string            `json:"id"`
+}
+
+// IsProlongableWith other checks that message can be prolonged with provided status message instead of reopening
+func (s *StatusMessage) IsProlongableWith(other *StatusMessage) bool {
+	categoriesEqual := slices.EqualUnordered(s.Categories, other.Categories)
+	return s.Type == other.Type && s.Heading == other.Heading && s.Message == other.Message && categoriesEqual && s.Code == other.Code
 }
