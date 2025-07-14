a5b1a7c5c4918e9c4f8d74a2797c0d2f63e28881
timmyb32r <timmyb32r@yandex-team.com>
2025-07-14 16:32:43 +0300

    fix flaky test 'transfer_manager/go/internal/logger/mutable_registry_test.go' -- TestMutableRegistry commit_hash:c0a81240f55a458e98d3784c238ae34a6fd30974

--- internal/logger/mutable_registry_test.go
+++ internal/logger/mutable_registry_test.go
@@ -1,12 +1,30 @@
 package logger
 
 import (
+	"encoding/json"
+	"sort"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 	"github.com/transferia/transferia/library/go/core/metrics/solomon"
 	"github.com/transferia/transferia/pkg/util/size"
 )
+
+func splitMetricsSortAndSerialize(in *solomon.Metrics) []string {
+	metricsArr := in.SplitToChunks(1)
+	sort.Slice(metricsArr, func(i, j int) bool {
+		l, _ := json.Marshal(metricsArr[i])
+		r, _ := json.Marshal(metricsArr[j])
+		return string(l) < string(r)
+	})
+
+	result := make([]string, 0)
+	for _, m := range metricsArr {
+		val, _ := json.Marshal(m)
+		result = append(result, string(val))
+	}
+	return result
+}
 
 func TestMutableRegistry(t *testing.T) {
 	solomonRegistry := solomon.NewRegistry(solomon.NewRegistryOpts().AddTags(map[string]string{"some_tag": "some_value"}))
@@ -40,7 +58,7 @@ func TestMutableRegistry(t *testing.T) {
 	// gather metrics with tags
 	metrics2, err := solomonRegistry.Gather()
 	require.NoError(t, err)
-	json2, err := metrics2.MarshalJSON()
-	require.NoError(t, err)
-	require.Equal(t, "{\"metrics\":[{\"type\":\"COUNTER\",\"labels\":{\"sensor\":\"some_counter\",\"some_other_tag\":\"some_other_value\",\"some_tag\":\"some_value\"},\"value\":11},{\"type\":\"HIST\",\"labels\":{\"sensor\":\"some_hist\",\"some_other_tag\":\"some_other_value\",\"some_tag\":\"some_value\"},\"hist\":{\"bounds\":[1024,5120,10240,51200,102400,512000,1048576,5242880,10485760,52428800,104857600,524288000,1073741824],\"buckets\":[1,0,0,0,0,0,0,0,0,0,0,0,0]}}]}", string(json2))
+	jsonArr := splitMetricsSortAndSerialize(metrics2)
+	require.Equal(t, `{"metrics":[{"type":"COUNTER","labels":{"sensor":"some_counter","some_other_tag":"some_other_value","some_tag":"some_value"},"value":11}]}`, jsonArr[0])
+	require.Equal(t, `{"metrics":[{"type":"HIST","labels":{"sensor":"some_hist","some_other_tag":"some_other_value","some_tag":"some_value"},"hist":{"bounds":[1024,5120,10240,51200,102400,512000,1048576,5242880,10485760,52428800,104857600,524288000,1073741824],"buckets":[1,0,0,0,0,0,0,0,0,0,0,0,0]}}]}`, jsonArr[1])
 }
