fe16b4328f34e8219b8a5da04990ada40d500f43
robot-piglet <robot-piglet@yandex-team.com>
2025-11-26 15:46:55 +0300

    Intermediate changes commit_hash:9836386729685a1c7150504702792133270815ff

--- library/go/core/metrics/mock/registry.go
+++ library/go/core/metrics/mock/registry.go
@@ -78,7 +78,10 @@ func (r Registry) FuncCounter(name string, function func() int64) metrics.FuncCo
 	metricName := r.newMetricName(name)
 	key := registryutil.BuildRegistryKey(metricName, r.tags)
 	s := &FuncCounter{function: function}
-	if _, loaded := r.metrics.LoadOrStore(key, s); loaded {
+	if val, loaded := r.metrics.LoadOrStore(key, s); loaded {
+		if r.allowLoadRegisteredMetrics {
+			return val.(*FuncCounter)
+		}
 		panic("metric with key " + key + " already registered")
 	}
 	return s
@@ -105,7 +108,10 @@ func (r Registry) FuncGauge(name string, function func() float64) metrics.FuncGa
 	metricName := r.newMetricName(name)
 	key := registryutil.BuildRegistryKey(metricName, r.tags)
 	s := &FuncGauge{function: function}
-	if _, loaded := r.metrics.LoadOrStore(key, s); loaded {
+	if val, loaded := r.metrics.LoadOrStore(key, s); loaded {
+		if r.allowLoadRegisteredMetrics {
+			return val.(*FuncGauge)
+		}
 		panic("metric with key " + key + " already registered")
 	}
 	return s
@@ -132,7 +138,10 @@ func (r *Registry) FuncIntGauge(name string, function func() int64) metrics.Func
 	metricName := r.newMetricName(name)
 	key := registryutil.BuildRegistryKey(metricName, r.tags)
 	s := &FuncIntGauge{function: function}
-	if _, loaded := r.metrics.LoadOrStore(key, s); loaded {
+	if val, loaded := r.metrics.LoadOrStore(key, s); loaded {
+		if r.allowLoadRegisteredMetrics {
+			return val.(*FuncIntGauge)
+		}
 		panic("metric with key " + key + " already registered")
 	}
 	return s
